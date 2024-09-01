2a77d9d2fe9653b7b7d5080f39f65ba49e311061
Shavinda Dissanayake <shavinda.diz@gmail.com>
2024-09-01 20:48:47 +0530

    Added #02 : Data Types

--- 1 Swift Basics/swift_basics.swift
+++ 1 Swift Basics/swift_basics.swift
@@ -1,7 +1,7 @@
 // Online Swift compiler to run Swift program online
 // (https://www.programiz.com/swift/online-compiler/)
 
-//#01 : Variables and Constants
+// #01 : Variables and Constants
 //Variables can change their values.
 var name = "Shavinda"
 name = "Max"
@@ -12,3 +12,13 @@ let age = 30
 // age = 30  // This will cause an error because 'age' is a constant
 
 print("\(name) \(age)")
+
+// #02 : Data Types
+//Swift is type-safe, meaning that it requires you to be explicit about the types of values your code can work with.
+
+var greeting: String = "Hello, World!"
+var year: Int = 2024
+var isSwiftFun: Bool = true
+let pi: Double = 3.14159
+print(pi)
+
