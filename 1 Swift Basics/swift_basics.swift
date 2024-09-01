4a8504e2d52722e10e371c832fdf0d9f8fc3fd8d
Shavinda Dissanayake <shavinda.diz@gmail.com>
2024-09-01 20:45:01 +0530

    Init swift basics section

---
+++ 1 Swift Basics/swift_basics.swift
@@ -0,0 +1,14 @@
+// Online Swift compiler to run Swift program online
+// (https://www.programiz.com/swift/online-compiler/)
+
+//#01 : Variables and Constants
+//Variables can change their values.
+var name = "Shavinda"
+name = "Max"
+print(name)
+
+//Constants cannot change once they are set.
+let age = 30
+// age = 30  // This will cause an error because 'age' is a constant
+
+print("\(name) \(age)")
